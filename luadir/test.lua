ea7020b0f918f7bc47f69eda720d329831867b41
wuxinyi <tongji_wxy@163.com>
2016-07-01 19:04:51 +0800

    fix bugs

--- luadir/test.lua
+++ luadir/test.lua
@@ -3,6 +3,9 @@ print(xml.xmlparser.test1[1].t1v)
 print(xml.xmlparser.test1[1].t2v)
 print(xml.xmlparser.test1[1].t3v)
 
+print(xml.xmlparser.test2[1].testn[1].id)
+print(xml.xmlparser.test2[1].testn[1].name)
+
 print(xml.xmlparser.test3.num1)
 print(xml.xmlparser.test3.num2)
 print(xml.xmlparser.test3.num3)
--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ int main()
 		return 1;
 	}
 
+	std::cout << "------------------------- C++ ------------------------------------" << std::endl;
 	for (auto &v : xml::xmlparser.test1)
 	{
 		std::cout << v.second.t1v << " " <<  v.second.t2v << " " << v.second.t3v << std::endl;
@@ -24,14 +25,51 @@ int main()
 	for (auto &v : xml::xmlparser.test2)
 	{
 		std::cout << v.id << " " <<  v.name << std::endl;
+    std::cout << "------" << std::endl;
+		for(xml::XMLPARSER::TEST2::TESTN_ConstIt it = v.testn.cbegin(); it != v.testn.cend(); ++it)
+		{
+			std::cout << it->second.id << " " << it->second.name << std::endl << std::endl;
+		}
 	}
-
 	std::cout << xml::xmlparser.test3.num1 << " " << xml::xmlparser.test3.num2 << " " << xml::xmlparser.test3.num3 << std::endl;
 
 	std::cout << "----------------------------LUA---------------------------------------" << std::endl;
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
   xml::lua_xmlparser.doLoad2luaState(L);	
-	luaL_dofile(L, "./luadir/test.lua");	
+	luaL_dofile(L, "./luadir/test.lua");
+
+	/*XMLDocument doc;
+	int ret = 0;
+	if((ret = doc.LoadFile("./xml/template.xml")) != XML_SUCCESS) {
+		std::cout << "error" << std::endl;
+		return false;
+	}
+
+	const XMLElement *ele = NULL;
+	ele = doc.FirstChildElement("xmlparser");
+	const XMLAttribute* attri = NULL;
+  attri = ele->FindAttribute("version"); 
+	std::cout << attri->Value() << std::endl;
+
+	const XMLElement *node=ele->FirstChildElement("test2"); 
+	const XMLElement *node_end=ele->LastChildElement("test2");
+	while(true)
+	{
+		if (strcmp(node->Name(), "test2") != 0) 
+		{
+			node = node->NextSiblingElement();
+			continue;
+		}
+		std::cout << node->Name() << std::endl;
+		const XMLAttribute* attri = NULL;
+		attri = node->FindAttribute("id"); 
+		std::cout << attri->Value() << std::endl;
+		attri = node->FindAttribute("name"); 
+		std::cout << attri->Value() << std::endl;
+		if (node == node_end) break;
+		node = node->NextSiblingElement();
+	}*/
+
 	return 0;
 }
